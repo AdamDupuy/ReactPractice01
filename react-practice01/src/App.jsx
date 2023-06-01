d1f81eba3d605eddccd0af44a538ef510296c79c
AdamDupuy <ajdupuy15@gmail.com>
2023-06-01 15:30:34 -0500

    Added header, decided on direction, updated documentation on clickup and decided on a design choice with the panels

--- react-practice01/src/App.jsx
+++ react-practice01/src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">
+                        Cool Manga Panels
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container mawWidth="sm">
+                        <Typography variant = "h2" align = "center" color = "textPrimary" gutterBottom>
+                            The Panels
+                        </Typography>
+                        <Typography variant = "h5" align = "center" color = "textSecondary" paragraph>
+                            Hello, this is my current manga panels site.  The following is an explanation of the themes of Power and Responsibility in the Manga titled "Helck." The series examines the relationship between power and responsibility. Helck, as one of the strongest beings in the world, struggles with the weight of his power and the responsibilities that come with it. It explores the moral dilemmas he faces and the choices he must make to protect those he cares about. The story reflects on the notion that power can be both a blessing and a burden, and emphasizes the importance of using it responsibly.
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+        </>
     );
 }
 
